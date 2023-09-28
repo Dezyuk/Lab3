f906127578703fea0f74295d716340984b2de349
Dezyuk <romanyuk.groz@gmail.com>
2023-09-28 13:29:33 +0300

    932

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace Lab3
             //task925();
             //Удалить столбец Двумерного массива вещественных чисел, в котором находится максимальный элемент этого массива.
 
-            //task932();
+            task932();
             //Отсортировать четные строки массива по возрастанию, а нечетные по убыванию.
 
             //task951();
@@ -88,8 +88,32 @@ namespace Lab3
             {
                 for (int j = 0; j < array2D.GetLength(1); j++)
                 {
-                    int a = i % 2 != 0 ? array2D.GetLength(0) - i : i;
-                    Console.Write(array2D[a, j] + "\t");
+                    for (var k = 0; k < array2D.GetLength(1); k++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            if (array2D[i, j] <= array2D[i, k]) continue;
+                            int temp = array2D[i, j];
+                            array2D[i, j] = array2D[i, k];
+                            array2D[i, k] = temp;
+                        }
+                        else
+                        {
+                            if (array2D[i, j] >= array2D[i, k]) continue;
+                            int temp = array2D[i, j];
+                            array2D[i, j] = array2D[i, k];
+                            array2D[i, k] = temp;
+                        }
+
+                    }
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < array2D.GetLength(0); i++)
+            {
+                for (int j = 0; j < array2D.GetLength(1); j++)
+                {
+                    Console.Write(array2D[i, j] + "\t");
                 }
                 Console.WriteLine();
             }
@@ -139,26 +163,26 @@ namespace Lab3
             {
                 for (int j = 0; j < array2D.GetLength(1); j++)
                 {
-                    averageValue+= array2D[i, j];
+                    averageValue += array2D[i, j];
                 }
             }
             averageValue /= (array2D.GetLength(0) * array2D.GetLength(1));
-            int xCoord =0, yCoord =0;
+            int xCoord = 0, yCoord = 0;
             double differenceWithAverageValue = Math.Abs(array2D[xCoord, yCoord] - averageValue);
             for (int i = 0; i < array2D.GetLength(0); i++)
             {
                 for (int j = 0; j < array2D.GetLength(1); j++)
                 {
-                    if(differenceWithAverageValue > Math.Abs(array2D[i, j] - averageValue))
+                    if (differenceWithAverageValue > Math.Abs(array2D[i, j] - averageValue))
                     {
                         differenceWithAverageValue = Math.Abs(array2D[i, j] - averageValue);
-                        xCoord=j;
-                        yCoord=i;
+                        xCoord = j;
+                        yCoord = i;
                     }
                 }
             }
             Console.WriteLine($"\nСреднее значение среди всех элементов массива:{averageValue}");
-            Console.WriteLine($"\nКоординаты(индекс) элемента, наиболее близкого к среднему значению всех элементов массива: x={xCoord}, y={yCoord}") ;
+            Console.WriteLine($"\nКоординаты(индекс) элемента, наиболее близкого к среднему значению всех элементов массива: x={xCoord}, y={yCoord}");
         }
 
     }
